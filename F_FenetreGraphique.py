2de3e7b73a89fe959b60014cc5ae7c8670978bad
GitRom320 <moim57016@gmail.com>
2026-05-13 17:29:27 +0200

    Ajout de l'interface graphique des courbes

--- F_FenetreGraphique.py
+++ F_FenetreGraphique.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
-from PyQt5.uic import loadUi
 import sys
+from PyQt5.uic import loadUi
+
 
 class Fenetregraphique(QMainWindow):
     def __init__(self):
@@ -8,3 +9,32 @@ class Fenetregraphique(QMainWindow):
         loadUi(r"F_Ui\F_graph_window.ui", self)
 
         self.F_quit_button.clicked.connect(self.close)
+
+        # Boutons de test
+        self.F_refresh_button.clicked.connect(self.test_actualiser)
+        self.F_export_button.clicked.connect(self.test_exporter)
+
+        # Texte affiché au démarrage de la fenêtre graphique
+        self.F_info_text.setPlainText("Fenêtre graphique ouverte.")
+
+    def test_actualiser(self):
+        """
+        Test du bouton Actualiser.
+        """
+
+        print("Bouton Actualiser cliqué")
+
+        self.F_info_text.appendPlainText(
+            "Test : le bouton Actualiser fonctionne."
+        )
+
+    def test_exporter(self):
+        """
+        Test du bouton Exporter.
+        """
+
+        print("Bouton Exporter cliqué")
+
+        self.F_info_text.appendPlainText(
+            "Test : le bouton Exporter fonctionne."
+        )
