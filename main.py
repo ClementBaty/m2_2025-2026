8fb10ad9ec47f8e20fae7a8c7696c41550dca083
unknown <tom.bottazzini@gmail.com>
2026-06-16 14:55:35 +0200

    Respect de la pep8 et des consigne projet, ajout de sécuriter sur le choix du fichier d'entrée

--- main.py
+++ main.py
@@ -55,13 +55,13 @@ Bonnes pratiques imposées :
 # ============================================================================
 # IMPORTS
 # ============================================================================
+import datetime
 import sys
 
 from PyQt5 import QtWidgets
-from Dialog import Ui_Dialog
 from PyQt5.QtWidgets import QFileDialog
 
-import datetime # Import nécessaire pour le timestamp
+from Dialog import Ui_Dialog
 
 # ============================================================================
 # CONSTANTES
@@ -81,18 +81,23 @@ class MainWindow(Ui_Dialog):
         self.setupUi(dialog)
         self.connexions()
 
-    """
-    La fonction connexions Permet de connecter les signalaux et Slots
 
-    Paramètres d'entrée :
-    -------
-    Aucun
-    
-    Effets
-    -------
-    Lors d'ppuis dans l'interface on appel des fonctions (slot)
-    """
     def connexions(self):
+        """
+        La fonction connexions Permet de connecter les signalaux et Slots
+
+        Paramètres d'entrée :
+        -------
+        Aucun
+        
+        Returns
+        -------
+        None
+        
+        Effets
+        -------
+        Lors d'ppuis dans l'interface on appel des fonctions (slot)
+        """
         # Connect the .clicked() signal with the .calculate() slot
         self.Generer.clicked.connect(self.calculate)
         # Connexion bouton
@@ -100,18 +105,40 @@ class MainWindow(Ui_Dialog):
         
 
     def calculate(self):
+        """
+        La fonction calculate Permet de réaliser le coeur du projet.
+        Lors de l'appuis sur le bouton générer cet fonction est appeler
+
+        Paramètres d'entrée :
+        -------
+        Aucun
+        
+        Returns
+        -------
+        None
+        
+        Effets
+        -------
+        
+        Extrais les donner 
+        Appel la fonction de géneration du fichier 
+        Réaliser le coeur du projet        
+        """
         # Nom et localisation du fichier à lire
         #nom_fichier = "C:\Temp\M2_Progr\m2_2025-2026\Fichier_entree.csv"
         nom_fichier = self.AffichageURL.text()
 
         # On crée un objet "mon_signal" à partir de la classe Mon_Signal_GrpA
-        mon_signal = Mon_Signal_GrpA()
+        mon_signal = MonSignalGrpA()
 
         # On appelle la méthode d'acquisition (Etape 1)
-        mon_signal.Acquisition_signal(nom_fichier)
+        try:
+            mon_signal.acquisition_signal(nom_fichier)
+        except Exception as e:
+            print(f"Erreur : {e}")
 
         # On appelle la méthode de traitement (Etape 2)
-        mon_signal.Traitement_signal()
+        mon_signal.traitement_signal()
         
         # A remplir par Manal
         mon_signal.type = self.comboBox_Type.currentText()
@@ -136,30 +163,36 @@ class MainWindow(Ui_Dialog):
         
         print(f"Aperçu des 5 premiers samples : {mon_signal.samples[:5]}")
 
-    """
-    La fonction ouvrir_fichier Permet d'ouvrir l'exporateur de fichier, pour sélection le fichier d'entrée'
 
-    Paramètres d'entrée :
-    -------
-    Aucun
-        
-    Effets
-    -------
-    Charge le fichier d'entrée
-    """
     def ouvrir_fichier(self):
+        """
+        La fonction ouvrir_fichier Permet d'ouvrir l'exporateur de fichier, pour sélection le fichier d'entrée
+        Un filtre est appliquer pour ouvrire uniquement un fichier .csv
+
+        Paramètres d'entrée :
+        -------
+        Aucun
+        
+        Returns
+        -------
+        None
+            
+        Effets
+        -------
+        Charge le fichier d'entrée
+        """
         file_path, _ = QFileDialog.getOpenFileName(
             None,
             "Choisir un fichier",
             "",
-            "Tous les fichiers (*.*)"
+            "Tous les fichiers (*csv*)"
         )
         
         if file_path:
             self.AffichageURL.setText(file_path)
 
 
-class Mon_Signal_GrpA:
+class MonSignalGrpA:
     
     # Le constructeur : il initialise les variables quand on crée le signal
     def __init__(self):
@@ -171,16 +204,20 @@ class Mon_Signal_GrpA:
         self.samples = []
         
         # Variable interne du vecteur temps du signal 
-        self.t = [] 
+        self.t = []
 
     # ETAPE 1 : EXTRACTION DES DONNEES
-    def Acquisition_signal(self, filename):
+    def acquisition_signal(self, filename):
         """
         La fonction Acquisition_signal lit le fichier CSV contenant le signal.
 
         Paramètres d'entrée :
         -------
         filename : str (chemin du fichier + nom du fichier)
+        
+        Returns
+        -------
+        None
 
         Effets
         -------
@@ -195,7 +232,7 @@ class Mon_Signal_GrpA:
         with open(filename, "r") as f:
             next(f)  # saute la première ligne (les en-têtes)
             for ligne in f:
-                ligne = ligne.strip()  # enlever les espaces et retours à la ligne
+                ligne = ligne.strip() #enlever les espaces et retours à la ligne
                 if not ligne: # Sécurité : ignorer les lignes vides
                     continue
                 valeurs = ligne.split(",")  # Le séparateur est une virgule
@@ -205,13 +242,17 @@ class Mon_Signal_GrpA:
                 self.samples.append(float(valeurs[1]))
 
     # ETAPE 2 : TRAITEMENT DES DONNEES POUR CALCULER LES INFORMATIONS DU SIGNAL
-    def Traitement_signal(self):
+    def traitement_signal(self):
         """
         La fonction Traitement_signal traite le signal.
 
         Paramètres d'entrée :
         -------
         Aucun
+        
+        Returns
+        -------
+        None
 
         Effets
         -------
