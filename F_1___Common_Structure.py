636948d21fdb8ac291c54b3e69a4220cbe224325
CKiLive <135030922+CKiLive@users.noreply.github.com>
2026-06-17 14:12:02 +0200

    modification init voyant circulraire

--- F_1___Common_Structure.py
+++ F_1___Common_Structure.py
@@ -5,23 +5,26 @@ class COMMONCLASS(QMainWindow):
     def go_to(self, nom_fenetre):
         self.fenetres[nom_fenetre].show()
         self.hide()
-    def init_voyant(self, widget, taille=20, forme="carre", couleur="red"):
+
+    def init_voyant(self, widget, taille=20, forme="rond", couleur="red"):
         widget.setFixedSize(taille, taille)
 
-        radius = 0
-        if forme == "rond":
-            radius = taille // 2
+        radius = taille // 2 if forme == "rond" else 0
+        widget.setProperty("radius", radius)
 
         widget.setStyleSheet(f"""
             background-color: {couleur};
             border: 2px solid black;
             border-radius: {radius}px;
         """)
+
     def set_voyant_color(self, widget, couleur):
+        radius = widget.property("radius")
+
         widget.setStyleSheet(f"""
             background-color: {couleur};
             border: 2px solid black;
-            border-radius: 0px;
+            border-radius: {radius}px;
         """)
 class COMONVAR:
     def __init__(self):
